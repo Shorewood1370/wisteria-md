0cda1a21e475bc88d9462ef554c892781dc75c43
Shorewood1370 <davidshorewood50@gmail.com>
2025-05-02 20:19:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ const toBool = x => x === 'true' || x === true;
 module.exports = {
 	SUDO: process.env.SUDO
 		? process.env.SUDO.split(',')
-		: ['2349112171078', '94703981512', '2349123721026'],
-	OWNER: process.env.OWNER || '2349112171078',
+		: ['2349112171078', '2348109243618', '2349012640680'],
+	OWNER: process.env.OWNER || '2348109243618',
 	PREFIX: process.env.PREFIX || '?',
 	MONGO_URI: process.env.MONGO_URI || '',
 	ANTIDELETE_IN_CHAT: toBool(process.env.ANTIDELETE_IN_CHAT) || false,
 	ANTI_DELETE: toBool(process.env.ANTI_DELETE) || true,
-	ANTILINK: process.env.ANTILINK || 'kick',
+	ANTILINK: process.env.ANTILINK || 'delete',
 	PAIR_NUMBER: '94703981512',
 	USE_PAIRING_CODE: true,
 	BOT_IMG: process.env.BOT_IMG || 'https://files.catbox.moe/z0k3fv.jpg',
